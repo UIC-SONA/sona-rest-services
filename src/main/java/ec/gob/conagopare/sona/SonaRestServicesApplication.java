7a06b8bc6060edee599627a7fce9eb8f6592580c
LUIDMIDEV <88100292+luidmidev@users.noreply.github.com>
2024-10-10 14:25:18 -0500

    first commit

---
+++ src/main/java/ec/gob/conagopare/sona/SonaRestServicesApplication.java
@@ -0,0 +1,13 @@
+package ec.gob.conagopare.sona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonaRestServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SonaRestServicesApplication.class, args);
+    }
+
+}
