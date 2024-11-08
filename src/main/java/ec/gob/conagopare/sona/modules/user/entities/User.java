edccededaf0aa1f041c1084412cc99c36e17c6d6
LUIDMIDEV <88100292+luidmidev@users.noreply.github.com>
2024-11-07 20:54:22 -0500

    ADD PROPETIES TO WORKFLOW

--- src/main/java/ec/gob/conagopare/sona/modules/user/entities/User.java
+++ src/main/java/ec/gob/conagopare/sona/modules/user/entities/User.java
@@ -1,22 +1,21 @@
 package ec.gob.conagopare.sona.modules.user.entities;
 
 
+import ec.gob.conagopare.sona.application.configuration.auditor.Auditable;
 import io.github.luidmidev.storage.PurgableStored;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDate;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Table(name = "sona_user")
-public class User implements PurgableStored {
+public class User extends Auditable implements PurgableStored {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
