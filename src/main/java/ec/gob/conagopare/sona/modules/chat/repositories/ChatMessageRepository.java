3fda4d9521f3a021af887cc19fa78a4545dd62b5
LUIDMIDEV <88100292+luidmidev@users.noreply.github.com>
2024-12-05 15:46:16 -0500

    CHAT IMPROVED

--- src/main/java/ec/gob/conagopare/sona/modules/chat/repositories/ChatMessageRepository.java
+++
@@ -1,8 +0,0 @@
-package ec.gob.conagopare.sona.modules.chat.repositories;
-
-import ec.gob.conagopare.sona.modules.chat.models.ChatMessage;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ChatMessageRepository extends MongoRepository<ChatMessage, String> {
-
-}
