5fd3ee323a9a6e6e88fdd0a96bbc3cb0252fa772
LUIDMIDEV <88100292+luidmidev@users.noreply.github.com>
2025-01-23 00:00:22 -0500

    OVERRIDE CONSTRAINT VALIDATION ERRORS RESPONSE

--- src/main/java/ec/gob/conagopare/sona/application/errors/GlobalExceptionHandler.java
+++ src/main/java/ec/gob/conagopare/sona/application/errors/GlobalExceptionHandler.java
@@ -2,7 +2,10 @@ package ec.gob.conagopare.sona.application.errors;
 
 
 import io.github.luidmidev.springframework.web.problemdetails.DefaultProblemDetailsExceptionHandler;
+import io.github.luidmidev.springframework.web.problemdetails.schemas.FieldMessage;
+import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.validator.internal.engine.path.PathImpl;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,6 +14,7 @@ import org.springframework.web.context.request.WebRequest;
 
 import java.sql.SQLIntegrityConstraintViolationException;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 @Slf4j
@@ -22,6 +26,22 @@ public class GlobalExceptionHandler extends DefaultProblemDetailsExceptionHandle
         var defaultDetail = "SQL Integrity Constraint Violation Exception.";
         log.warn("Se recibió una excepción de SQLIntegrityConstraintViolationException, se recomienda revisar la integridad de los datos, la excepción es: {}", ex.getMessage());
         return createDefaultResponseEntity(ex, new HttpHeaders(), INTERNAL_SERVER_ERROR, defaultDetail, null, null, request);
+    }
+
+    @Override
+    public ResponseEntity<Object> handleConstraintViolationException(ConstraintViolationException ex, WebRequest request) {
+        var defaultDetail = "One or more fields are invalid.";
+        var body = super.createProblemDetail(ex, BAD_REQUEST, defaultDetail, null, null, request);
+
+        addValidationErrors(body, ex.getConstraintViolations(), violation -> {
+            var path = violation.getPropertyPath();
+            if (path instanceof PathImpl pathImpl) {
+                return new FieldMessage(pathImpl.getLeafNode().getName(), violation.getMessage());
+            }
+            return new FieldMessage(path.toString(), violation.getMessage());
+        });
+
+        return createResponseEntity(ex, new HttpHeaders(), BAD_REQUEST, request, body);
     }
 
 }
